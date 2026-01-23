f2159c27b4c0a4f340d175418ea51a2bbbdc62ba
Allen Guo <guosonglin001@gmail.com>
2026-01-23 23:26:29 +0800

    add tools & pixi

---
+++ lib/Tools/triton-shared-opt/RegisterTritonSharedDialects.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "mlir/Dialect/Bufferization/IR/Bufferization.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/Dialect/Linalg/Passes.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/Ptr/IR/PtrDialect.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
+
+#include "triton/Dialect/Triton/IR/Dialect.h"
+#include "triton/Dialect/Triton/Transforms/Passes.h"
+
+#include "triton-shared/Conversion/StructuredToMemref/Passes.h"
+#include "triton-shared/Conversion/TritonArithToLinalg/Passes.h"
+#include "triton-shared/Conversion/TritonPtrToMemref/Passes.h"
+#include "triton-shared/Conversion/TritonToLinalg/Passes.h"
+#include "triton-shared/Conversion/TritonToLinalgExperimental/Passes.h"
+#include "triton-shared/Conversion/TritonToStructured/Passes.h"
+#include "triton-shared/Conversion/TritonToUnstructured/Passes.h"
+#include "triton-shared/Conversion/UnstructuredToMemref/Passes.h"
+#include "triton-shared/Dialect/TPtr/IR/TPtrDialect.h"
+#include "triton-shared/Dialect/TritonStructured/IR/TritonStructuredDialect.h"
+#include "triton-shared/Dialect/TritonTilingExt/IR/TritonTilingExtDialect.h"
+#include "triton-shared/Transform/AddLLVMDebugInfo/Passes.h"
+
+#include "mlir/InitAllPasses.h"
+
+inline void registerTritonSharedDialects(mlir::DialectRegistry &registry) {
+  mlir::registerAllPasses();
+  mlir::registerLinalgPasses();
+  mlir::triton::registerTritonPasses();
+  mlir::triton::registerTritonToLinalgPass();
+  mlir::triton::registerTritonToLinalgExperimentalPasses();
+  mlir::triton::registerTritonToStructuredPass();
+  mlir::triton::registerTritonPtrToMemref();
+  mlir::triton::registerUnstructuredToMemref();
+  mlir::triton::registerTritonToUnstructuredPasses();
+  mlir::triton::registerTritonArithToLinalgPasses();
+  mlir::triton::registerStructuredToMemrefPasses();
+  mlir::triton::registerAddLLVMDebugInfoPass();
+
+  // TODO: register Triton & TritonGPU passes
+  registry.insert<
+      mlir::tptr::TPtrDialect, mlir::ptr::PtrDialect,
+      mlir::ttx::TritonTilingExtDialect, mlir::tts::TritonStructuredDialect,
+      mlir::triton::TritonDialect, mlir::cf::ControlFlowDialect,
+      mlir::math::MathDialect, mlir::arith::ArithDialect, mlir::scf::SCFDialect,
+      mlir::linalg::LinalgDialect, mlir::func::FuncDialect,
+      mlir::tensor::TensorDialect, mlir::memref::MemRefDialect,
+      mlir::bufferization::BufferizationDialect>();
+}
---
+++ lib/Tools/triton-shared-opt/triton-shared-opt.cpp
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+//
+//===----------------------------------------------------------------------===//
+
+#include "RegisterTritonSharedDialects.h"
+
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+  registerTritonSharedDialects(registry);
+
+  return mlir::asMainReturnCode(
+      mlir::MlirOptMain(argc, argv, "Triton-Shared test driver\n", registry));
+}
