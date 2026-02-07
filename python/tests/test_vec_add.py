cddf0d98c4cc1476ae74caf1b55d2e60fb2e92ef
Allen Guo <guosonglin001@gmail.com>
2026-02-07 11:13:16 +0800

    tests

---
+++ python/tests/test_vec_add.py
@@ -0,0 +1,82 @@
+import torch
+
+import triton
+import triton.language as tl
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+
+@triton.jit
+def add_kernel(
+    x_ptr,  # *Pointer* to first input vector.
+    y_ptr,  # *Pointer* to second input vector.
+    output_ptr,  # *Pointer* to output vector.
+    n_elements,  # Size of the vector.
+    BLOCK_SIZE: tl.constexpr,  # Number of elements each program should process.
+    # NOTE: `constexpr` so it can be used as a shape value.
+):
+    # There are multiple 'programs' processing different data. We identify which program
+    # we are here:
+    pid = tl.program_id(axis=0)  # We use a 1D launch grid so axis is 0.
+    # This program will process inputs that are offset from the initial data.
+    # For instance, if you had a vector of length 256 and block_size of 64, the programs
+    # would each access the elements [0:64, 64:128, 128:192, 192:256].
+    # Note that offsets is a list of pointers:
+    block_start = pid * BLOCK_SIZE
+    offsets = block_start + tl.arange(0, BLOCK_SIZE)
+    # Create a mask to guard memory operations against out-of-bounds accesses.
+    mask = offsets < n_elements
+    # Load x and y from DRAM, masking out any extra elements in case the input is not a
+    # multiple of the block size.
+    x = tl.load(x_ptr + offsets, mask=mask)
+    y = tl.load(y_ptr + offsets, mask=mask)
+    output = x + y
+    # Write x + y back to DRAM.
+    tl.store(output_ptr + offsets, output, mask=mask)
+
+
+def add(x: torch.Tensor, y: torch.Tensor, output: torch.Tensor, BLOCK_SIZE):
+    # We need to preallocate the output.
+    # output = torch.empty_like(x)
+    # assert x.is_cuda and y.is_cuda and output.is_cuda
+    n_elements = output.numel()
+    # The SPMD launch grid denotes the number of kernel instances that run in parallel.
+    # It is analogous to CUDA launch grids. It can be either Tuple[int], or Callable(metaparameters) -> Tuple[int].
+    # In this case, we use a 1D grid where the size is the number of blocks:
+    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
+    # NOTE:
+    #  - Each torch.tensor object is implicitly converted into a pointer to its first element.
+    #  - `triton.jit`'ed functions can be indexed with a launch grid to obtain a callable GPU kernel.
+    #  - Don't forget to pass meta-parameters as keywords arguments.
+    # add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE, force_simt_only=True)
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE)
+    # We return a handle to z but, since `torch.cuda.synchronize()` hasn't been called, the kernel is still
+    # running asynchronously at this point.
+    return output
+
+
+def print_lastn(tensor, prefix, lastn=32):
+    last_32 = tensor.flatten()[-32:]
+    # last_32 = tensor.flatten()[:32]
+    formatted_str = ", ".join([f"{x:.2f}" for x in last_32.tolist()])
+    print(f"{prefix}: [{formatted_str}]", flush=True)
+
+
+def test_add(BLOCK_SIZE, log=False):
+    device = DEVICE
+    size = 8192 * 8 + 8192 * 5
+    dtype = torch.float32
+    x = torch.ones(size, device=device, dtype=dtype)
+    y = torch.ones(size, device=device, dtype=dtype)
+    output = torch.zeros(size, device=device, dtype=dtype)
+    output_torch = x + y
+    if log:
+        print_lastn(output_torch, "output_torch")
+    output_triton = add(x, y, output, BLOCK_SIZE)
+    if log:
+        print_lastn(output_triton, "output_triton")
+    torch.testing.assert_close(output_triton.to("cpu"), output_torch.to("cpu"))
+
+
+if __name__ == "__main__":
+    test_add(BLOCK_SIZE=8192, log=True)
