aa0c23d7f8c814939a0cf6e54519205d850b0d35
Allen Guo <guosonglin001@gmail.com>
2026-02-11 21:37:55 +0800

    wip

--- python/tb/bench.py
+++ python/tb/bench.py
@@ -1,10 +1,15 @@
 import shlex
 
+import _wrap_api  # noqa
+
 from tritonbench.utils.parser import get_parser
 from tritonbench.operators import load_opbench_by_name
 
 
-args_sh = "--op vector_add --only torch_add"
+# args_sh = "--op vector_add --only torch_add"
+args_sh = "--op vector_add --only triton_ --only-match-mode prefix-with-baseline"
+# args_sh = "--op layer_norm --only triton_fused_layer_norm --only-match-mode exact"
+# args_sh = "--op layer_norm --only triton_ --only-match-mode prefix-with-baseline"
 
 args_sh += " -d cpu --mode fwd_no_grad --dtype fp32"
 
