92a14de6750e046978a6be6b19824e6f67290f85
Allen Guo <guosonglin001@gmail.com>
2026-02-11 21:01:29 +0800

    bench

--- python/tb/bench.py
+++ python/tb/bench.py
@@ -3,12 +3,15 @@ import shlex
 from tritonbench.utils.parser import get_parser
 from tritonbench.operators import load_opbench_by_name
 
-args = "-d cpu --mode fwd_no_grad --dtype fp32"
 
+args_sh = "--op vector_add --only torch_add"
+
+args_sh += " -d cpu --mode fwd_no_grad --dtype fp32"
+
+args = shlex.split(args_sh)
 parser = get_parser()
-op_args = parser.parse_args(shlex.split(args))
+op_args = parser.parse_args(args)
 
-addmm_bench = load_opbench_by_name("vector_add")(op_args)
+addmm_bench = load_opbench_by_name(op_args.op)(op_args)
 
-# TODO: fix err
 addmm_bench.run()
