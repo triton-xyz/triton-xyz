8fe1e397a04fcf533ec71105eb483281b82febc6
Allen Guo <guosonglin001@gmail.com>
2026-06-04 23:14:43 +0800

    align build

--- utils/agent/build_cmake.sh
+++ utils/agent/build_cmake.sh
@@ -1,19 +1,19 @@
 ###############################################################################
 
+if [[ ! -e "$PWD/third_party/triton/patch.patch" ]]; then
+  ln -s $PWD/third_party/triton_patch/CMakePresets.json $PWD/third_party/triton/
+  ln -s $PWD/third_party/triton_patch/patch.patch $PWD/third_party/triton/
+
+  pushd $PWD/third_party/triton
+  git apply patch.patch
+  popd
+fi
+
 rm -rf build/CMakeFiles
 rm -rf build/CMakeCache.txt
 [[ "$(uname)" == "Darwin" ]] && PRESET="osx_lld" || PRESET="osx"
 cmake --preset $PRESET -S$PWD/third_party/triton -B$PWD/build \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DTRITON_CODEGEN_BACKENDS="nvidia;amd" \
-  -DTRITON_PLUGIN_DIRS=$PWD \
-  -DCMAKE_INSTALL_PREFIX=$PWD/build/install \
-  -DCMAKE_RUNTIME_OUTPUT_DIRECTORY=$PWD/build/bin \
-  -DTRITON_WHEEL_DIR=$PWD/build/bin \
-  -DLLVM_SYSPATH=$PWD/llvm-triton/llvm-project/build \
-  -DLLVM_DIR=$PWD/llvm-triton/llvm-project/build/lib/cmake/llvm \
-  -DMLIR_DIR=$PWD/llvm-triton/llvm-project/build/lib/cmake/mlir \
-  -DLLD_DIR=$PWD/llvm-triton/llvm-project/build/lib/cmake/lld \
   -DCMAKE_C_COMPILER=$(which clang) \
   -DCMAKE_CXX_COMPILER=$(which clang++) \
   -DPython3_EXECUTABLE=$(which python)
